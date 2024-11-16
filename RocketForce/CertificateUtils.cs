9b9951f4f75c1d90319464cca7ad86bc82d2763f
Acidus <acidus99@users.noreply.github.com>
2024-11-15 23:22:17 -0500

    Methods to try/load a certificate and generate certificate

--- RocketForce/CertificateUtils.cs
+++ RocketForce/CertificateUtils.cs
@@ -2,55 +2,72 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 
 namespace RocketForce;
 
 public static class CertificateUtils
 {
     /// <summary>
-    /// Attempts to load an RSA or ECDSA certificate.
+    /// Attempts to load a certificate, given a certificate file ane private key
     /// </summary>
-    public static X509Certificate2 LoadCertificate(string certFilePath, string privateRSAKeyFilePath)
+    /// <param name="certPath"></param>
+    /// <param name="keyPath"></param>
+    /// <param name="certificate"></param>
+    /// <returns>true if certificate could be loaded</returns>
+    public static bool TryLoadCertificate(string certPath, string keyPath, out X509Certificate2? certificate)
     {
-        X509Certificate2 cert = new X509Certificate2(certFilePath);
-        string[] lines = File.ReadAllLines(privateRSAKeyFilePath, Encoding.UTF8);
-        string privateKeyData = "";
-        for (int i = 1; i < lines.Length - 1; ++i)
-        {
-            privateKeyData += lines[i];
-        }
-        byte[] bytes = Convert.FromBase64String(privateKeyData);
-
-        //try RSA
+        certificate = null;
         try
         {
-            RSA rsa = RSA.Create();
-            // For now we always expect to have cert with BEGIN PRIVATE KEY label
-            // TODO: add handling for other types, maybe??
-            rsa.ImportPkcs8PrivateKey(bytes, out _);
-
-            X509Certificate2 pubPrivEphemeral = cert.CopyWithPrivateKey(rsa);
-            return new X509Certificate2(pubPrivEphemeral.Export(X509ContentType.Pfx));
+            certificate = X509Certificate2.CreateFromPemFile(certPath, keyPath);
+            return true;
         }
         catch (Exception)
         {
+            return false;
         }
+    }
 
-        //try ECDSA
+    public static bool CreateLocalCertificates(string certPath, string keyPath, string subjectName)
+    {
+        // Create an elliptic curve (EC) key pair with P-256 curve
         try
         {
-            ECDsa ecdsa = ECDsa.Create();
-            // For now we always expect to have cert with BEGIN PRIVATE KEY label
-            // TODO: add handling for other types, maybe??
-            ecdsa.ImportPkcs8PrivateKey(bytes, out _);
+            using (ECDsa ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256))
+            {
+                // Define the certificate request
+                var certRequest = new CertificateRequest(subjectName, ecdsa, HashAlgorithmName.SHA256);
 
-            X509Certificate2 pubPrivEphemeral = cert.CopyWithPrivateKey(ecdsa);
-            return new X509Certificate2(pubPrivEphemeral.Export(X509ContentType.Pfx));
+                // Add certificate extensions (e.g., key usage, subject key identifier)
+                certRequest.CertificateExtensions.Add(
+                    new X509BasicConstraintsExtension(false, false, 0, false));
+                certRequest.CertificateExtensions.Add(
+                    new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, true));
+                certRequest.CertificateExtensions.Add(
+                    new X509SubjectKeyIdentifierExtension(certRequest.PublicKey, false));
+
+                // Generate the self-signed certificate with a 5-year expiration
+                X509Certificate2 certificate =
+                    certRequest.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
+
+                string privateKeyPem = ToPem(ecdsa.ExportPkcs8PrivateKey(), "PRIVATE KEY");
+                string publicKeyPem = ToPem(certificate.Export(X509ContentType.Cert), "CERTIFICATE");
+                File.WriteAllText(keyPath, privateKeyPem);
+                File.WriteAllText(certPath, publicKeyPem);
+                return true;
+            }
         }
         catch (Exception)
         {
+            return false;
         }
-        throw new ApplicationException("Could not load valid certificate");
+    }
+    
+    // Helper method to convert a byte array to PEM format
+    private static string ToPem(byte[] data, string type)
+    {
+        // PEM format headers and footers
+        string base64 = Convert.ToBase64String(data, Base64FormattingOptions.InsertLineBreaks);
+        return $"-----BEGIN {type}-----\n{base64}\n-----END {type}-----";
     }
 }
